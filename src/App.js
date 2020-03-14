efa45947957b33546bcafdcac7f604c956b87918
google <test>
2020-03-14 15:18:01 +0300

    API

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import React, { Component } from "react";
+import ClipLoader from "react-spinners/ClipLoader";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+  state = {
+    loading: true,
+    person: null
+  };
+
+  async componentDidMount() {
+    const url = "https://api.randomuser.me/";
+    const response = await fetch(url);
+    const date = await response.json();
+    this.setState({ person: date.results[0], loading: false });
+    //this.setState({ person: null, loading: false });
+  }
+  render() {
+    if (this.state.loading) {
+      return (
+        <center>
+          <div className="sweet-loading">
+            <br></br>
+            <br></br>
+            <ClipLoader
+              size={150}
+              color={"grey"}
+              loading={this.state.loading}
+            />
+          </div>
+        </center>
+      );
+    }
+    if (!this.state.person) {
+      return (
+        <div>
+          <center>Didn't get a person </center>{" "}
+        </div>
+      );
+    }
+    return (
+      <div>
+        <br></br>
+        <br></br>
+        <center>
+          <div>
+            <div> {this.state.person.name.title} </div>
+            <div> {this.state.person.name.first} </div>
+            <div> {this.state.person.name.last} </div>
+            <img
+              src={this.state.person.picture.large}
+              height="150"
+              width="150"
+            />
+          </div>
+        </center>
+      </div>
+    );
+  }
 }
-
-export default App;
